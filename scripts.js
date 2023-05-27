1d5e9e34a334384d3bd1f30c37a4420ae94738ad
Ignacio <ignacio06cornejo@gmail.com>
2023-05-27 18:49:25 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+class Gifts {
+  constructor(name, relationships, age, preferences) {
+    this.name = name;
+    this.relationships = relationships;
+    this.age = age;
+    this.preferences = preferences;
+  }
+}
